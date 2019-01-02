499f0a40ff9dfc096181909bc483e59a21ad661d
sebastian <sebastianh.16592@gmail.com>
2019-01-02 16:48:19 -0500

    Se crean los roles de usuario

--- resources/views/ahorro/lista.blade.php
+++ resources/views/ahorro/lista.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+
+@if(Auth::user()->hasRole('admin'))
+    <div>Acceso como administrador</div>
+@else
+    <div>Acceso usuario</div>
+@endif
 <div class="container">
     <div class="row">
         @forelse ($ahorros as $message)
